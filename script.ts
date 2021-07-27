ed1efb603c55574b8d4e2b87c739de8ad0d0c486
hrhr49 <hiro49410@gmail.com>
2021-07-28 00:04:03 +0900

    implement.

--- script.ts
+++ script.ts
@@ -1,38 +1,100 @@
+import './style.css';
+
 const sizes = [72, 96, 120, 128, 144, 152, 180, 192, 384, 512];
 const fileInput: HTMLInputElement = document.forms['file-form'].elements['file-input'];
 
-const addImageAndDownloadButton = (file: File, width: number, height: number) => {
+const addImageAndDownloadButton = async (file: File, width: number, height: number) => {
   // 画像をリサイズする
   const image = new Image();
   const reader = new FileReader();
-  reader.onload = function(e) {
-    image.onload = function() {
-      // サムネ描画用canvasのサイズを上で算出した値に変更
-      const canvas: HTMLCanvasElement = document.getElementById('canvas') as HTMLCanvasElement;
+
+  reader.readAsDataURL(file);
+
+  const imgSrc: string = await new Promise<string>((resolve) => {
+    reader.onload = (e: ProgressEvent<FileReader>) => {
+      resolve(e.target.result.toString());
+    }
+  });
+
+  image.src = imgSrc;
+
+  const canvas = await new Promise<HTMLCanvasElement>((resolve) => {
+    image.onload = () => {
+      const canvas: HTMLCanvasElement = document.createElement('canvas');
       canvas.setAttribute('width', width.toString());
       canvas.setAttribute('height', height.toString());
 
       var ctx = canvas.getContext('2d');
+
       // canvasに既に描画されている画像をクリア
-      ctx.clearRect(0,0,width,height);
-      // canvasにサムネイルを描画
-      ctx.drawImage(image,0,0,image.width,image.height,0,0,width,height);
+      ctx.clearRect(0, 0, width, height);
+      // canvasに画像を描画
+      ctx.drawImage(image, 0, 0, image.width, image.height, 0, 0, width, height);
+      resolve(canvas);
     }
-    image.src = e.target.result.toString();
-  }
-  reader.readAsDataURL(file);
+  });
+
+  const downloadButton = document.createElement('button');
+  downloadButton.textContent = `download icon-${width}x${height}.png`;
+
+  downloadButton.addEventListener('click', () => {
+    const a = document.createElement("a");
+    a.href = canvas.toDataURL("image/png");
+    a.setAttribute("download", `icon-${width}x${height}.png`);
+    a.dispatchEvent(new MouseEvent("click"));
+  });
+
+  return {
+    canvas,
+    downloadButton,
+  };
 };
 
-fileInput.addEventListener('change', () => {
-  const file: File = fileInput.files[0];
-
+const onFileUpload = async (file: File) => {
   if (!['image/jpeg', 'image/jpg', 'image/png'].includes(file.type)) {
     alert('cannot read file. jpeg or png is supported.')
     return;
   }
 
-  const THUMBNAIL_WIDTH = 600; // 画像リサイズ後の横の長さの最大値
-  const THUMBNAIL_HEIGHT = 600; // 画像リサイズ後の縦の長さの最大値
+  const div = document.getElementById('image-area');
 
-  addImageAndDownloadButton(file, THUMBNAIL_WIDTH, THUMBNAIL_HEIGHT);
+  // remove previous result
+  div.textContent = '';
+
+  for (const size of sizes) {
+    const {canvas, downloadButton} = await addImageAndDownloadButton(file, size, size);
+    const div2 = document.createElement('div');
+    const div3 = document.createElement('div');
+    const div4 = document.createElement('div');
+    div2.appendChild(canvas);
+    div3.appendChild(downloadButton);
+    div4.appendChild(div2);
+    div4.appendChild(div3);
+    div4.style.border = 'solid 1px blue';
+    div.appendChild(div4);
+  }
+}
+
+fileInput.addEventListener('change', async () => {
+  const file: File = fileInput.files[0];
+  await onFileUpload(file);
+});
+
+const uploadArea = document.getElementById('upload-area') as HTMLDivElement;
+
+uploadArea.addEventListener('dragover', (e) => {
+  e.preventDefault();
+  uploadArea.classList.add('drag');
+});
+
+uploadArea.addEventListener('dragleave', () => {
+  uploadArea.classList.remove('drag');
+});
+
+uploadArea.addEventListener('drop', (e) => {
+  e.preventDefault();
+  uploadArea.classList.remove('drag');
+  const files = e.dataTransfer.files;
+  fileInput.files = files;
+  onFileUpload(files[0]);
 });
